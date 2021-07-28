dd39275925f0382905a65597b9a49bf6745eeba7
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2021-07-28 18:50:27 -0300

    questoes 2,4,6,8

---
+++ questao02.py
@@ -0,0 +1,8 @@
+def multiple(a,b):
+    if a%b == 0:
+        return True
+    else:
+        return False
+
+r  = multiple(35,7)
+print(r)
---
+++ questao04.py
@@ -0,0 +1,6 @@
+def triangle_area(b,a):
+    return (b*a)/2
+
+area = triangle_area(10,4)
+
+print(area)
